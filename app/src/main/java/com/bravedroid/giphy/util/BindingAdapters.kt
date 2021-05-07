4c95f8f254186f3e0cd5e3c774907addb40a90a9
houssemzaier <houssem.zaier@gmail.com>
2021-05-07 21:26:21 +0200

    fix bug databinding

--- app/src/main/java/com/bravedroid/giphy/util/BindingAdapters.kt
+++ app/src/main/java/com/bravedroid/giphy/util/BindingAdapters.kt
@@ -4,9 +4,11 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
+
 @BindingAdapter("app:verticalStaggeredGridLayoutSpanCount")
-fun RecyclerView.setVerticalStaggeredGridLayout(spanCount : Int?){
-   layoutManager= StaggeredGridLayoutManager(spanCount!!,RecyclerView.VERTICAL).apply {
-        gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
-    }
+fun setVerticalStaggeredGridLayout(recyclerView: RecyclerView, spanCount: Int) {
+    recyclerView.layoutManager =
+        StaggeredGridLayoutManager(spanCount, RecyclerView.VERTICAL).apply {
+            gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+        }
 }
